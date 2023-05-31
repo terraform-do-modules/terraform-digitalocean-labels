381013a0fdfbb958df38896b84c1c34c773d80b1
mahesh yadav <devopsmahesh.it@gmail.com>
2023-05-31 20:44:12 +0530

    fix: update versions.tf file

--- _examples/complete/versions.tf
+++ _examples/complete/versions.tf
@@ -1,8 +1,10 @@
+# Terraform version
 terraform {
+  required_version = ">= 0.13"
   required_providers {
     digitalocean = {
-      source = "digitalocean/digitalocean"
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
     }
   }
-  required_version = ">= 0.13"
 }
