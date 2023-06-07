e7ab51035cc8a6376b549ecba9a107b168c645f6
mahesh yadav <devopsmahesh.it@gmail.com>
2023-06-07 20:53:12 +0530

    fix: update tf checks workflows

--- _examples/complete/versions.tf
+++ _examples/complete/versions.tf
@@ -1,11 +1,10 @@
 # Terraform version
 terraform {
   required_providers {
+    required_version = ">= 1.4.6"
     digitalocean = {
       source  = "digitalocean/digitalocean"
       version = "~> 2.0"
     }
   }
 }
-
-
--- versions.tf
+++ versions.tf
@@ -1,5 +1,6 @@
 # Terraform version
 terraform {
+  required_version = ">= 1.4.6"
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
